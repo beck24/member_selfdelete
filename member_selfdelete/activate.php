c57fa920eaf9f4e11141b475c69d873b32cfab66
Matt Beckett <beck24@gmail.com>
2012-04-07 19:42:27 -0600

    fixes issue with whitescreen during activation on some systems

--- member_selfdelete/activate.php
+++ member_selfdelete/activate.php
@@ -1,10 +1,12 @@
 <?php
 
 // set default values if nothing is set
-if(empty(elgg_get_plugin_setting('feedback', 'member_selfdelete'))){
+$feedback = elgg_get_plugin_setting('feedback', 'member_selfdelete'); 
+if(empty($feedback)){
   elgg_set_plugin_setting('feedback', 'yes', 'member_selfdelete');
 }
 
-if(empty(elgg_get_plugin_setting('method', 'member_selfdelete'))){
+$method = elgg_get_plugin_setting('method', 'member_selfdelete'); 
+if(empty($method)){
   elgg_set_plugin_setting('method', 'delete', 'member_selfdelete');
 }
